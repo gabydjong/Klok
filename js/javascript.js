a87ad64315d9162d73ac2ccdcf8ca5b4a242b806
Gaby de Jong <gabydejong@mbp-van-gaby.home>
2020-03-16 22:07:46 +0100

    sterren

--- js/javascript.js
+++ js/javascript.js
@@ -3,11 +3,11 @@ var today = new Date();
 document.getElementById("specifiek").innerHTML = today.toString();
 
   function klok (){
-  var uren =document.getElementById('uren');
+  var uren = document.getElementById('uren');
   var minuten = document.getElementById ('minuten');
   var seconden = document.getElementById('seconden');
 
-  var u =  8; // new Date().getHours();
+  var u = new Date().getHours();
   var m = new Date().getMinutes();
   var s = new Date().getSeconds();
 
@@ -22,52 +22,76 @@ document.getElementById("specifiek").innerHTML = today.toString();
     
   /* achtergrond kleur verandering en elementen veranderen */
 
-    if(u >= 8 && u < 19){
+    if(u >= 8 && u <= 19){
     	document.body.classList.add("overdag");
 
       /* laat de zon zien en laat hem stil staan, en laat de maan niet zien*/
-      element = document.querySelector('.zon'); 
-      element.style.visibility = 'visible'; 
-      element.style.animation = 'none';
+      zonelement = document.querySelector('.zon'); 
+      zonelement.style.visibility = 'visible'; 
+      zonelement.style.animation = 'none';
 
-      ele = document.querySelector('.maan'); 
-      ele.style.visibility = 'hidden'; 
+      maanelement = document.querySelector('.maan'); 
+      maanelement.style.visibility = 'hidden'; 
 
       /* Ruimteschip eerder langs laten gaan */
       document.getElementById("ruimteschip").style.animationDelay = "3s";
 
+      /* sterren zijn niet zichtbaar*/
+      sterelement = document.querySelector('.container-stars'); 
+      sterelement.style.visibility = 'hidden'; 
+
     }
     
-    else if(u >= 19 && u <= 20){
+    else if(u > 19 && u <= 20){
     	document.body.classList.add("geeloranje");
 
       /* laat de zon zien, en niet de maan*/
-      element = document.querySelector('.zon'); 
-      element.style.visibility = 'visible'; 
+      zonelementelement = document.querySelector('.zon'); 
+      zonelementelement.style.visibility = 'visible'; 
 
-      ele = document.querySelector('.maan'); 
-      ele.style.visibility = 'hidden'; 
+      maanelement = document.querySelector('.maan'); 
+      maanelement.style.visibility = 'hidden'; 
 
       /* zon laten vertragen */
       document.getElementById("zon").style.animationDuration = "500s";
+
+      /* sterren zijn niet zichtbaar*/
+      sterelement = document.querySelector('.container-sterren'); 
+      sterelement.style.visibility = 'hidden'; 
+
+      /* sterren zijn niet zichtbaar*/
+      sterelement = document.querySelector('.container-sterren'); 
+      sterelement.style.visibility = 'hidden'; 
 
     }  
 
     else if(u > 20 && u <= 21){
       document.body.classList.add("overgang");
 
+      /* sterren zijn niet zichtbaar*/
+      sterelement = document.querySelector('.container-stars'); 
+      sterelement.style.visibility = 'hidden'; 
+
     } 
 
     else{
 		  document.body.classList.add("nacht");
       /* laat de maan zien, en niet de zon*/
-      element = document.querySelector('.zon'); 
-      element.style.visibility = 'hidden'; 
+      zonelement = document.querySelector('.zon'); 
+      zonelement.style.visibility = 'hidden'; 
       document.getElementById("maan").style.opacity = "0.9";
 
-      ele = document.querySelector('.maan'); 
-      ele.style.visibility = 'visible'; 
-      ele.style.animation = 'none';
+      maanelement = document.querySelector('.maan'); 
+      maanelement.style.visibility = 'visible'; 
+      maanelement.style.animation = 'none';
+
+      /* Te donker om wolken te zien */
+      wolkelement = document.querySelector('.sky'); 
+      wolkelement.style.visibility = 'hidden'; 
+
+      /* sterren zijn nu wel zichtbaar */
+      sterelement = document.querySelector('.container-sterren'); 
+      sterelement.style.visibility = 'visible'; 
 
     }
 
