e335c0e9510c0f548b9100b48808f1add5b314bc
Gaby de Jong <gabydejong@mbp-van-gaby.home>
2020-03-16 19:20:56 +0100

    elementenoptijd

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@
 var today = new Date();
 document.getElementById("specifiek").innerHTML = today.toString();
 
-function klok (){
+  function klok (){
   var uren = document.getElementById('uren');
   var minuten = document.getElementById ('minuten');
   var seconden = document.getElementById('seconden');
@@ -20,17 +20,55 @@ function klok (){
   minuten.innerHTML = m;
   seconden.innerHTML = s;
     
-  /* Verandering basis op tijd*/
+  /* achtergrond kleur verandering en elementen veranderen */
 
-    if(u >= 8 && u < 20){
+    if(u >= 8 && u < 19){
     	document.body.classList.add("overdag");
+
+      /* laat de zon zien en laat hem stil staan, en laat de maan niet zien*/
+      element = document.querySelector('.zon'); 
+      element.style.visibility = 'visible'; 
+      element.style.animation = 'none';
+
+      ele = document.querySelector('.maan'); 
+      ele.style.visibility = 'hidden'; 
+
+      /* Ruimteschip eerder langs laten gaan */
+      document.getElementById("ruimteschip").style.animationDelay = "3s";
+
     }
-    else if(u >= 20 && u <= 21){
+    
+    else if(u >= 19 && u <= 20){
     	document.body.classList.add("geeloranje");
-    }    
+
+      /* laat de zon zien, en niet de maan*/
+      element = document.querySelector('.zon'); 
+      element.style.visibility = 'visible'; 
+
+      ele = document.querySelector('.maan'); 
+      ele.style.visibility = 'hidden'; 
+
+      /* zon laten vertragen */
+      document.getElementById("zon").style.animationDuration = "500s";
+
+    }  
+
+    else if(u > 20 && u <= 21){
+      document.body.classList.add("overgang");
+
+    } 
+
     else{
-		document.body.classList.add("nacht");
-    document.classList.add("zon");
+		  document.body.classList.add("nacht");
+      /* laat de maan zien, en niet de zon*/
+      element = document.querySelector('.zon'); 
+      element.style.visibility = 'hidden'; 
+      document.getElementById("maan").style.opacity = "0.9";
+
+      ele = document.querySelector('.maan'); 
+      ele.style.visibility = 'visible'; 
+      ele.style.animation = 'none';
+
     }
 
 
